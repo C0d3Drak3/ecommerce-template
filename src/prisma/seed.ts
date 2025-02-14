c0a8ae43ff9f816057979996a37e4003b8435ea1
Agustin Galvan <agustingalvan02@gmail.com>
2025-02-14 12:23:33 -0300

    Prisma origin fixed

--- src/prisma/seed.ts
+++ src/prisma/seed.ts
@@ -1,9 +1,12 @@
 // src/prisma/seed.ts
-import { prisma } from '../lib/prisma';
+import { PrismaClient } from '@prisma/client';
 import fs from "fs";
 import path from "path";
+import { fileURLToPath } from 'url';
 
-
+const prisma = new PrismaClient();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 // Define una interfaz para tipar los datos que vienen del JSON
 interface ProductData {
   title: string;
@@ -35,7 +38,7 @@ async function main() {
           stock: product.stock,
           category: product.category,
           thumbnail: product.thumbnail,
-          brand: product.brand,
+          brand: product.brand || "Default Brand",
           tags: product.tags
         },
       });
@@ -43,8 +46,10 @@ async function main() {
     console.log("Seeding completed successfully!");
   } catch (error) {
     console.error("Error seeding database:", error);
-  } finally {
+  }   if (typeof prisma.$disconnect === 'function') {
     await prisma.$disconnect();
+  } else {
+    console.error('⚠️ prisma.$disconnect no es una función. Revisa la instancia de prisma.');
   }
 }
 
